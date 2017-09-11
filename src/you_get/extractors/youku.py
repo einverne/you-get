dfea15b9e1f6a80bbaa4d2b5ef10062828ac4417
Ein Verne <einverne@gmail.com>
2017-09-11 11:10:19 +0800

    Fix youku add random ua
    Signed-off-by: Ein Verne <einverne@gmail.com>

--- src/you_get/extractors/youku.py
+++ src/you_get/extractors/youku.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import json
+import time
+import urllib.parse
+import urllib.request
+
 from ..common import *
 from ..extractor import VideoExtractor
 
-import time
-import traceback
-import json
-import urllib.request
-import urllib.parse
-
 
 def fetch_cna():
-
     def quote_cna(val):
         if '%' in val:
             return val
@@ -36,9 +34,64 @@ def fetch_cna():
     return quote_cna('DOG4EdW4qzsCAbZyXbU+t7Jt')
 
 
+import random
+
+
+class UserAgent:
+    agent = {}
+
+    def random(self):
+        self.get_platform()
+        self.get_os()
+        self.get_browser()
+
+        if self.agent['browser'] == 'Chrome':
+            webkit = str(random.randint(500, 599))
+            version = "%s.0%s.%s" % (str(random.randint(0, 24)), str(random.randint(0, 1500)), str(random.randint(0, 999)))
+
+            return "Mozilla/5.0 (%s) AppleWebKit/%s.0 (KHTML, live Gecko) Chrome/%s Safari/%s" % (self.agent['os'], webkit, version, webkit)
+        elif self.agent['browser'] == 'Firefox':
+            year = str(random.randint(2000, 2015))
+            month = str(random.randint(1, 12)).zfill(2)
+            day = str(random.randint(1, 28)).zfill(2)
+            gecko = "%s%s%s" % (year, month, day)
+            version = "%s.0" % (str(random.randint(1, 15)))
+
+            return "Mozillia/5.0 (%s; rv:%s) Gecko/%s Firefox/%s" % (self.agent['os'], version, gecko, version)
+        elif self.agent['browser'] == 'IE':
+            version = "%s.0" % (str(random.randint(1, 10)))
+            engine = "%s.0" % (str(random.randint(1, 5)))
+            option = random.choice([True, False])
+            if option:
+                token = "%s;" % (random.choice(['.NET CLR', 'SV1', 'Tablet PC', 'Win64; IA64', 'Win64; x64', 'WOW64']))
+            else:
+                token = ''
+
+            return "Mozilla/5.0 (compatible; MSIE %s; %s; %sTrident/%s)" % (version, self.agent['os'], token, engine)
+
+    def get_os(self):
+        if self.agent['platform'] == 'Machintosh':
+            self.agent['os'] = random.choice(['68K', 'PPC'])
+        elif self.agent['platform'] == 'Windows':
+            self.agent['os'] = random.choice(
+                ['Win3.11', 'WinNT3.51', 'WinNT4.0', 'Windows NT 5.0', 'Windows NT 5.1', 'Windows NT 5.2', 'Windows NT 6.0',
+                 'Windows NT 6.1', 'Windows NT 6.2', 'Win95', 'Win98', 'Win 9x 4.90', 'WindowsCE'])
+        elif self.agent['platform'] == 'X11':
+            self.agent['os'] = random.choice(['Linux i686', 'Linux x86_64'])
+
+    def get_browser(self):
+        self.agent['browser'] = random.choice(['Chrome', 'Firefox', 'IE'])
+
+    def get_platform(self):
+        self.agent['platform'] = random.choice(['Machintosh', 'Windows', 'X11'])
+
+
+ua = UserAgent()
+
+
 class Youku(VideoExtractor):
     name = "优酷 (Youku)"
-    mobile_ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36'
+    mobile_ua = ua.random()
     dispatcher_url = 'vali.cp31.ott.cibntv.net'
 
     # Last updated: 2015-11-24
@@ -286,6 +339,7 @@ def youku_download_by_url(url, **kwargs):
 
 def youku_download_by_vid(vid, **kwargs):
     Youku().download_by_vid(vid, **kwargs)
+
 
 download = youku_download_by_url
 download_playlist = youku_download_playlist_by_url
